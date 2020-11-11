cd372672293f57e55d882cb4b3022cea49f281ef
Mahmood Abu Awwad <abuawwadmahmood@gmail.com>
2020-11-11 10:29:17 +0200

    :) -> :(((((

--- app/routes.py
+++ app/routes.py
@@ -2,22 +2,26 @@ from app import app
 from flask_cors import CORS
 from flask import request
 from flask import Flask
-from flaskext.mysql import MySQL
 import os, json
+import mysql.connector
 
 
 
 CORS(app)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-mysql = MySQL()
 
-app.config['MYSQL_HOST'] = os.environ['HOST']
-app.config['MYSQL_PORT'] = os.environ['PORT']
-app.config['MYSQL_USER'] = 'flask'
-app.config['MYSQL_PASSWORD'] = 'flask'
-app.config['MYSQL_DB'] = 'backend'
-app.config['MYSQL_ROOT_PASSWORD'] = 'flask'
-mysql.init_app(app)
+
+# app.config['MYSQL_HOST'] = '192.168.204.226'#os.environ['HOST']
+# app.config['MYSQL_PORT'] = os.environ['PORT']
+# app.config['MYSQL_USER'] = 'flask'
+# app.config['MYSQL_PASSWORD'] = 'flask'
+# app.config['MYSQL_DB'] = 'backend'
+# app.config['MYSQL_ROOT_PASSWORD'] = 'flask'
+# mysql.init_app(app)
+
+
+db = mysql.connector.connect(user='flask', password='flask',host='192.168.204.226',database='backend')
+
 
 
 @app.route('/api/v1/add/<name>',methods=['GET', 'POST'])
@@ -29,11 +33,11 @@ def add(name):
         # conn = sqlite3.connect('todoss.db')
         # c = conn.cursor()
 
-        cur = mysql.connect().cursor()
+        cur = db.cursor()
         cur.execute("CREATE TABLE IF NOT EXISTS todo_db (todo text);")
         cur.execute("INSERT INTO todo_db(todo) VALUES(?);",(name,))
-        mysql.connection.commit()
-        cur.close()
+        db.commit()
+        db.close()
        
          # conn.commit()
         # conn.close()
@@ -46,12 +50,13 @@ def fetch():
     # conn = sqlite3.connect('todoss.db')
     # c = conn.cursor()
     # c.execute("CREATE TABLE IF NOT EXISTS todo_db (todo text);")
-    c = mysql.connect().cursor()
+    c = db.cursor()
     c.execute("CREATE TABLE IF NOT EXISTS todo_db (todo text);")
     c.execute("SELECT * FROM todo_db;")
     data = c.fetchall()
-    mysql.connection.commit()
-    c.close()
+    db.commit()
+    db.close()
+
           
     # conn.commit()
     # conn.close()
